78b556838130018f12905f0cc038889506fd696c
georgayang <945454050@qq.com>
2023-07-07 00:39:29 +0800

    leetcode hot 100 _ 20

--- code/test.go
+++ code/test.go
@@ -8,8 +8,81 @@ import (
 func main() {
 	// testChan()
 	// testNewMake()
-	testReflect()
+	// testReflect()
+	result := isMatch("aa","a*")
+	fmt.Println(result)
 }
+
+
+func isMatch(s string, p string) bool {
+	// dp[i][j] ---  s的前i个字符 能否匹配p的前j个字符
+	// dp[0][0] --- 1
+	// dp[0][j] --- 如果 p[j] 是 * 且dp[i][j-2]为1 
+	// dp[i][0] --- 0 
+	// dp[i][j] --- 
+	lenS, lenP := len(s), len(p)
+	dp := make([][]uint8, lenS + 1)
+	for i := range dp {
+		dp[i] = make([]uint8, lenP + 1)
+	}
+
+	// fmt.Println(dp)
+	dp[0][0] = 1
+	// dp[0][j] --- 如果 p[j] 是 * 且dp[i][j-2]为1 
+	for j:= 2; j <= lenP; j ++ {
+		if p[j - 1] == '*' {
+			dp[0][j] = dp[0][j - 2]
+		}
+	}
+
+	for i := 1; i <= lenS; i ++ {
+		for j := 1; j <= lenP; j ++ {
+			// 当前两个字符相同 
+			if s[i - 1] == p[j - 1] {
+				dp[i][j] = dp[i - 1][j - 1]
+			} else {
+				switch p[j - 1] {
+				case '*': {
+					// fmt.Printf("case '*': i:%d j:%d p[j-2]:%s s[i-1]:%s\n", i, j, string(p[j - 2]),  string(s[i - 1]))
+					if j >= 2 {
+                        if p[j - 2] == '.' || p[j - 2] == s[i - 1] {
+							// fmt.Println(" if p[j - 2] == '.' || p[j - 2] == s[i - 1] ")
+							val1 := dp[i-1][j] // p最后的 x*可以顶替s末尾的x  如果 这之前的s和p都匹配上了 那肯定没问题
+							val2 := dp[i][j-2]	// p最后的 x*可以当作不存在  如果此时s已经可以和p末尾出去x*的部分匹配 那也没问题
+						
+							if val1 == 1 || val2 == 1 {
+								dp[i][j] = 1
+							}
+                        } else {
+                            dp[i][j] = dp[i][j - 2]
+                        }
+					} else {
+                        dp[i][j] = 0
+                    }
+				}
+				case '.': {
+					dp[i][j] = dp[i - 1][j - 1]
+				}
+				default: {
+					dp[i][j] = 0
+				}
+				}
+			}
+
+		}
+	}
+
+	for i:=0; i <=lenS; i ++ {
+		fmt.Println(dp[i])
+	}
+	return dp[lenS][lenP] == 1
+}
+
+
+
+
+
+
 
 type  Animal interface {
 	Say()
@@ -47,7 +120,7 @@ func handle(animal Animal) {
 	// 如果是狗  就让它叫
 	// 如果是猫  就让它吃
 
-	val := reflect.ValueOf(animal)
+	// val := reflect.ValueOf(animal)
 	typ := reflect.TypeOf(animal)
 
 	nameOfType := typ.Name()
---
+++ leetcode/romanToInt_test.go
@@ -0,0 +1,404 @@
+package leetcode_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSolve(t *testing.T) {
+	// ret := romanToInt("MCMXCIV")
+	// ret := longestCommonPrefix([]string{"fl1232123123ower","fl123123123ow","fl123123123ight"})
+	// ret  := threeSum([]int {-1,0,1,2,-1,-4,-2,-3,3,0,4})
+	// threeSumClosest([]int {0,0,0}, 1)
+	// letterCombinations("23")
+	// fourSum([]int{1,0,-1,0,-2,2}, 0)
+	// removeNthFromEnd([]int{1,0,-1,0,-2,2}, 0)
+	// head := &ListNode{
+	// 	Val: 1,
+	// 	Next: &ListNode{
+	// 		Val: 2,
+	// 		Next: nil,
+	// 		// &ListNode{
+	// 		// 	Val: 3, 
+	// 		// 	Next: &ListNode{
+	// 		// 		Val: 4,
+	// 		// 		Next: nil,
+	// 		// 	},
+	// 		// },
+	// 	},
+	// }
+	// removeNthFromEnd(head, 1)
+	isValid("[][]({[]})")
+	// fmt.Println(ret)
+}
+
+func isValid(s string) bool {
+	if len(s) % 2 == 1 || len(s) == 0 {
+		return false 
+	}
+	stack := []byte{}
+	braceMap := map[byte]byte {
+		'(': ')',
+		'{': '}',
+		'[': ']',
+	}
+	for i:=0; i < len(s); i++ {
+		if len(stack) == 0 || s[i] != stack[len(stack) - 1] {
+			right, exist := braceMap[s[i]]
+			if !exist {
+				return false
+			}
+			stack = append(stack, right)
+		} else {
+			stack = stack[0:len(stack) - 1]
+		}
+	}
+
+	return len(stack) == 0
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	curr := head
+	target := head
+	var pre *ListNode
+	diff := 0
+
+	for {
+		if diff == n - 1 {
+			if curr.Next == nil {
+				// pre 连到 curr
+				if pre == nil {
+					return target.Next
+				} else {
+					pre.Next = target.Next
+					return head
+				}
+			} else {
+				pre = target
+				target = target.Next 
+			}
+		} else {
+			// 还没达到目标差距 target不动
+			diff += 1
+		}
+		curr = curr.Next
+	}
+
+	// 当curr为nil时  此时的
+	return nil
+}
+
+
+func fourSum(nums []int, target int) [][]int {
+	sort(&nums)
+	var res [][]int
+
+	for i := 0; i < len(nums) - 3; i ++ {
+		if i > 0 && nums[i - 1] == nums[i] {
+			continue
+		}
+
+		for j := i + 1; j < len(nums) - 2; j ++ {
+			if j > i + 1 && nums[j - 1] == nums[j] {
+				continue
+			}
+
+			l, r := j + 1, len(nums) - 1
+
+			if nums[i] + nums[j] > target && nums[l] >= 0 {
+				continue
+			}
+
+			newTarget := target - nums[i] - nums[j]
+
+			for l < r {
+				if nums[l] + nums[r] == newTarget {
+					res = append(res, []int{nums[i], nums[j], nums[l], nums[r]})
+
+					// l右  r左
+					l += 1
+					for l < r {
+						if nums[l - 1] == nums[l]  {
+							l += 1
+							continue
+						}
+						break
+					}
+					r -= 1
+					for l < r {
+						if nums[r + 1] == nums[r]  {
+							r -= 1
+							continue
+						}
+						break
+					}
+				}
+
+				if nums[l] + nums[r] < newTarget {
+					// 小了  l往右走
+					l += 1
+					for l < r {
+						if nums[l - 1] == nums[l]  {
+							l += 1
+							continue
+						}
+						break
+					}
+				} else if nums[l] + nums[r] > newTarget {
+					// 大了  r往左走
+					r -= 1
+					for l < r {
+						if nums[r + 1] == nums[r]  {
+							r -= 1
+							continue
+						}
+						break
+					}
+				}
+			}
+		}
+	}
+
+	fmt.Println(res)
+	return res
+}
+
+func letterCombinations(digits string) []string {
+	if digits == "" {
+		return []string{}
+	}
+	
+	// res[i]  表示 从第i个按键开始的所有组合  是一个数组
+	// res[0] 就是结果 
+	// res[len - 1] = 3或4个字符 （对应按键上的四个字符）
+	// res[i] = digits[i]上的3或4个字符分别追加上res[i + 1]中的所有元素
+
+	keyMap := map[string][]string {
+		"2": {"a", "b", "c"}, 
+		"3": {"d", "e", "f"}, 
+		"4": {"g", "h", "i"}, 
+		"5": {"j", "k", "l"}, 
+		"6": {"m", "n", "o"}, 
+		"7": {"p", "q", "r", "s"}, 
+		"8": {"t", "u", "v"}, 
+		"9": {"w", "x", "y", "z"}, 
+	}
+	res := make([][]string, len(digits))
+
+	res[len(digits) - 1] = keyMap[string(digits[len(digits) - 1])]
+	
+	for i := len(digits) - 2; i > -1 ;i -- {
+		for j := range res[i + 1] {
+			currKeyList := keyMap[string(digits[i])]
+			for k:=0; k < len(currKeyList); k ++ {
+				res[i] = append(res[i], currKeyList[k] + res[i + 1][j])
+			}
+		}
+	}
+
+
+	// fmt.Println(res)
+	return res[0]
+}
+
+func threeSumClosest(nums []int, target int) int {
+	minDiff := 999999
+	minDiffVal := 0
+	sort(&nums)
+	for i:=0; i < len(nums) - 2; i ++ {
+		l, r := i + 1, len(nums) - 1
+
+
+
+		for l < r {
+			currDiff := abs(nums[i] + nums[l] + nums[r] - target)
+			// 找到一组可行解 
+			if abs(currDiff) < minDiff {
+				minDiff = currDiff
+				minDiffVal = nums[i] + nums[l] + nums[r]
+
+			}
+			if nums[i] + nums[l] + nums[r] > target {
+				// 大了  r往左走
+				r -= 1
+				for r > l {
+					if nums[r] == nums[r + 1] {
+						r -= 1
+					} else {
+						break
+					}
+				}
+			} else if nums[i] + nums[l] + nums[r] < target {
+				// 小了  l往右走
+				l += 1
+				for l < len(nums) {
+					if nums[l] == nums[l - 1] {
+						l += 1
+					} else {
+						break
+					}
+				}
+			} else {
+				// 已经完美相等  没必要再找了
+				return target
+			}
+		}
+
+		for i + 1 < len(nums) && nums[i + 1] == nums [i] {
+			i += 1
+		}
+	}
+
+	fmt.Println(minDiff)
+	fmt.Println(minDiffVal)
+
+
+	return minDiffVal
+}
+
+func abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -1 * a
+}
+
+func threeSum(nums []int) [][]int {
+	sort(&nums)
+	result := make([][]int, 0)
+	fmt.Println(nums)
+	l, r := 1, len(nums) - 1
+	for i:=0; i < len(nums) && nums[i] <= 0; i ++ {
+		l = i + 1
+		r = len(nums) - 1
+		for l < r {
+			// 找到一组可行解
+			if nums[l] + nums[r] + nums[i] == 0 {
+				result = append(result, []int{nums[i], nums[l], nums[r]})
+				// l往右走 这里要跳过与可行解中l相同的元素
+				for l + 1 < len(nums) {
+					if nums[l + 1] == nums[l] {
+						l += 1
+					} else {
+						l += 1
+						break
+					}
+				}
+				// l变大了  r一定要变小  往左走
+				for r > l {
+					if nums[r-1] == nums[r] {
+						r -= 1
+					} else {
+						r -= 1
+						break
+					}
+					
+				}
+			} else if nums[l] + nums[r] + nums[i] > 0 {
+				// 已经大于0了 只能r往左走
+				for r > l {
+					if nums[r-1] == nums[r] {
+						r -= 1
+					} else {
+						r -= 1
+						break
+					}
+				}
+			} else if nums[l] + nums[r] + nums[i] < 0 {
+				// 小于0 l可以往右走 r不动
+				for l + 1< len(nums) {
+					if nums[l + 1] == nums[l] {
+						l += 1
+					} else {
+						l += 1
+						break
+					}
+				}
+			}
+		}
+		for i + 1 < len(nums) && nums[i + 1] == nums[i] {
+			i += 1
+		}
+	}
+
+
+	return result
+} 
+
+func sort(list *[]int) {
+	for i:=0; i < len(*list) - 1; i ++ {
+		for j := i + 1; j < len(*list); j ++ {
+			if (*list)[i] > (*list)[j] {
+				tmp := (*list)[i]
+				(*list)[i] = (*list)[j]
+				(*list)[j] = tmp
+			}
+		}
+	}
+	fmt.Println(list)
+}
+
+func longestCommonPrefix(strs []string) string {
+	minLen := minLength(strs)
+	ret := ""
+	for i:=0; i < minLen; i ++ {
+		currChar := strs[0][i]
+
+		for j := 1; j < len(strs); j ++ {
+			if strs[j][i] != currChar {
+				return ret
+			}
+		}
+		ret += string(currChar)
+	}
+
+	return ret
+}
+
+func minLength(strs []string) int {
+	min := 200
+	for i := range strs {
+		if len(strs[i]) < min {
+			min =  len(strs[i])
+		}
+	}
+	return min
+}
+
+
+func romanToInt(s string) int {
+	valMap := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+
+	sum := 0
+	for i :=len(s) - 1; i > -1; i -- {
+		// currChar := string(s[i])
+		if i < len(s) - 1 && valMap[s[i]] < valMap[s[i+1]] {
+			sum -= valMap[s[i]]
+		} else {
+			sum += valMap[s[i]]
+		}
+	}
+
+	return sum
+}
